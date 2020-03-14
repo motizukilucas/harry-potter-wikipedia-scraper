26ceef0f7204075b6be1e6ea15c5289e1610a8e6
Lucas Motizuki <motizuki@protonmail.com>
2020-03-14 14:50:47 -0300

    script update

--- script.py
+++ script.py
@@ -6,3 +6,6 @@ r = requests.get('https://en.wikipedia.org/wiki/List_of_Harry_Potter_characters'
 soup = BeautifulSoup(r.text, 'html.parser')
 
 print(soup.prettify())
+
+with open('result.html', 'w') as file:
+    file.write(soup.prettify().encode('utf8'))
