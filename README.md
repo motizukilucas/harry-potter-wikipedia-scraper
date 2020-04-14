41eff98dfe2c9e382e12127d70b89d878ce94a2a
Lucas Motizuki <motizuki@protonmail.com>
2020-04-14 16:51:06 -0300

    Getting character links

--- README.md
+++ README.md
@@ -3,10 +3,6 @@
     <li> – <a href="https://en.wikipedia.org/wiki/Dumbledore%27s_Army#Hannah_Abbott" title="Dumbledore's Army">Hannah Abbott</a><a href="https://en.wikipedia.org/wiki/Hufflepuff" class="mw-redirect" title="Hufflepuff">Hufflepuff</a> student in <a href="https://en.wikipedia.org/wiki/Harry_Potter_(character)" title="Harry Potter (character)">Harry Potter</a>'s year. Prefect and member of <a href="https://en.wikipedia.org/wiki/Dumbledore%27s_Army" title="Dumbledore's Army">Dumbledore's Army</a>.</li>
 > However some of them don't have an a tag enclosing their names, which lead to the script collecting unwanted information.
 
-exaplin actual situation
-
-save link to characters pages
-
 manully parse resulsts
 
 access links with loop and collect data
--- script.py
+++ script.py
@@ -11,14 +11,22 @@ for x in range(0, 4):
     first_ul = first_ul.find_next("ul")
 
 with open('result.txt', 'w') as file:
+    
+    # saves all characters names
+    # for x in range(0, 23):
+    #     li_item = first_ul.li
+    #     for x in range(0, 20):
+    #         if(li_item.a):
+    #             file.write(li_item.a.string.encode('utf8'))
+    #             file.write("\n")
+    #             print(li_item.a.string.encode('utf8'))
+    #         li_item = li_item.find_next("li")
+    #     first_ul = first_ul.find_next("ul")
+
     for x in range(0, 23):
         li_item = first_ul.li
-
         for x in range(0, 20):
             if(li_item.a):
-                file.write(li_item.a.string.encode('utf8'))
-                file.write("\n")
-                print(li_item.a.string.encode('utf8'))
+                print(li_item.a.get('href').encode('utf8'))
             li_item = li_item.find_next("li")
-
         first_ul = first_ul.find_next("ul")
