f0f00aa778f921e436737441b70ac640df421f17
Lucas Motizuki <motizuki@protonmail.com>
2020-04-14 17:14:30 -0300

    Saving all the characters links in a variable

--- script.py
+++ script.py
@@ -23,10 +23,22 @@ with open('result.txt', 'w') as file:
     #         li_item = li_item.find_next("li")
     #     first_ul = first_ul.find_next("ul")
 
+    char_links = [240]
     for x in range(0, 23):
         li_item = first_ul.li
-        for x in range(0, 20):
+        for y in range(0, 20):
             if(li_item.a):
-                print(li_item.a.get('href').encode('utf8'))
+                # print(li_item.a.get('href').encode('utf8'))
+                # char_links[10*x+y] = str(li_item.a.get('href').encode('utf8'))
+                char_links.append(str(li_item.a.get('href').encode('utf8')))
+                print(char_links[10*x+y])
+                # print(10*x+y)
             li_item = li_item.find_next("li")
         first_ul = first_ul.find_next("ul")
+
+    # loop char link
+    #   se nao tiver # hastag salve na nova array && se tiver _ underline salve na nova sarray
+    #     acesar o link 
+    #     se for diferente de https://en.wikipedia.org + link
+    #         salvar na array novos links
+
